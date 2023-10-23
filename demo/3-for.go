9ad798258e3d04b54d026362b8fa5b9ea461a838
tianyang <34160429+tianyang-git@users.noreply.github.com>
2023-10-24 00:39:59 +0800

    go example

--- demo/3-for.go
+++ demo/3-for.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	i := 1
+	for i < 3 {
+		res := fmt.Sprintf("loop %d", i)
+		fmt.Println(res)
+		i = i + 1
+	}
+
+	for j := 7; j < 9; j++ {
+		fmt.Println(j)
+	}
+
+	for {
+		fmt.Println("loop for")
+		break
+	}
+
+	for n := 0; n < 6; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
 }
