87b42c1ad4d26fecf2ad6898929565afe710d948
tianyang <34160429+tianyang-git@users.noreply.github.com>
2023-11-04 23:07:13 +0800

    go example

--- src/35-worker-pools.go
+++ src/35-worker-pools.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for j := range jobs {
+		fmt.Println("before start len", len(jobs))
+		fmt.Println("worker", id, "started  job", j, "len", len(jobs))
+		time.Sleep(time.Second)
+		fmt.Println()
+		fmt.Println("worker", id, "finished job", j, "len", len(jobs))
+		results <- j * 2
+	}
+}
+
+func main() {
+	start := time.Now() // 获取当前时间
+
+	const numJobs = 5
+	jobs := make(chan int, numJobs)
+	results := make(chan int, numJobs)
+
+	for w := 1; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
+
+	for j := 1; j <= numJobs; j++ {
+		jobs <- j
+	}
+	close(jobs)
+	fmt.Println("close jobs")
+	for a := 1; a <= numJobs; a++ {
+		<-results
+	}
+
+	elapsed := time.Since(start)
+	fmt.Println("该函数执行完成耗时：", elapsed)
 }
