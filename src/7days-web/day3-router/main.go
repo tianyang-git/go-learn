4b31e0b6587d73c82bc9eb81badc494c7559cd76
tianyang <34160429+tianyang-git@users.noreply.github.com>
2023-11-12 19:25:36 +0800

    7days-web

--- src/7days-web/day3-router/main.go
+++ src/7days-web/day3-router/main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
-	"day3-context/gee"
+	"day3-router/gee"
 	"net/http"
 )
 
+// addRoute -> Engine wait
+// http -> handle(getRoute -> func)
 func main() {
-
 	r := gee.New()
 	r.GET("/", func(c *gee.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
 	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=geektutu
 		c.String(http.StatusOK, "Hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+	r.GET("/hello/:name", func(c *gee.Context) {
+		// expect /hello/geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+	r.GET("/assets/*filepath", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
 	})
 	r.POST("/login", func(c *gee.Context) {
 		c.JSON(http.StatusOK, gee.H{
