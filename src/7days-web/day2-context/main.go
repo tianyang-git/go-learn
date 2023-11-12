ac7f3a1b2197baf6d1f4da782269247acdaef19e
tianyang <34160429+tianyang-git@users.noreply.github.com>
2023-11-12 14:46:37 +0800

    7days-web

---
+++ src/7days-web/day2-context/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"day3-context/gee"
+	"net/http"
+)
+
+func main() {
+
+	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+	r.GET("/hello", func(c *gee.Context) {
+		c.String(http.StatusOK, "Hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+	r.POST("login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+	r.Run(":9999")
+}
