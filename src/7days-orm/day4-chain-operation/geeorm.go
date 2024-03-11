882a7b9bafcea87a6d29c3b3a2806f40389edd4d
tianyang <34160429+tianyang-git@users.noreply.github.com>
2024-03-12 00:06:15 +0800

    7days-orm

---
+++ src/7days-orm/day4-chain-operation/geeorm.go
@@ -0,0 +1,47 @@
+package day4_chain_operation
+
+import (
+	"database/sql"
+	"day4_chain_operation/dialect"
+	"day4_chain_operation/log"
+	"day4_chain_operation/session"
+)
+
+type Engine struct {
+	db      *sql.DB
+	dialect dialect.Dialect
+}
+
+func NewEngine(driver, source string) (e *Engine, err error) {
+	db, err := sql.Open(driver, source)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	// Send a ping to make sure the database connection is alive
+	if err = db.Ping(); err != nil {
+		log.Error(err)
+		return
+	}
+	// make sure the specific dialect exists
+	dial, ok := dialect.GetDialect(driver)
+	if !ok {
+		log.Errorf("dialect %s Not Found", driver)
+		return
+	}
+
+	e = &Engine{db: db, dialect: dial}
+	log.Info("Connect database success")
+	return
+}
+
+func (e *Engine) Close() {
+	if err := e.db.Close(); err != nil {
+		log.Error("Failed to close database")
+	}
+	log.Info("Close database success")
+}
+
+func (e *Engine) NewSession() *session.Session {
+	return session.New(e.db, e.dialect)
+}
