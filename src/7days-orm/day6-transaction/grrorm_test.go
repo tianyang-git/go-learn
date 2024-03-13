0f2e318eb71be284f1bb0a2c668050aebbe8ec29
tianyang <34160429+tianyang-git@users.noreply.github.com>
2024-03-13 22:54:11 +0800

    7days-orm

---
+++ src/7days-orm/day6-transaction/grrorm_test.go
@@ -0,0 +1,70 @@
+package day4_chain_operation
+
+import (
+	"day6_transaction/session"
+	"errors"
+	_ "github.com/mattn/go-sqlite3"
+	"testing"
+)
+
+type User struct {
+	Name string `geeorm:"PRIMARY KEY"`
+	Age  int
+}
+
+func OpenDb(t *testing.T) *Engine {
+	t.Helper()
+	engine, err := NewEngine("sqlite3", "gee.db")
+	if err != nil {
+		t.Fatal("failed to connect", err)
+	}
+	return engine
+}
+
+func TestNewEngine(t *testing.T) {
+	engine := OpenDb(t)
+	defer engine.Close()
+}
+
+func TestEngine_Transaction(t *testing.T) {
+	t.Run("rollback", func(t *testing.T) {
+		transactionRollback(t)
+	})
+	t.Run("commit", func(t *testing.T) {
+		transactionCommit(t)
+	})
+}
+
+func transactionRollback(t *testing.T) {
+	engine := OpenDb(t)
+	defer engine.Close()
+	s := engine.NewSession()
+	_ = s.Model(&User{}).DropTable()
+	_, err := engine.Transaction(func(s *session.Session) (result interface{}, err error) {
+		_ = s.Model(&User{}).CreateTable()
+		_, err = s.Insert(&User{"Tom", 18})
+		return nil, errors.New("error")
+	})
+	if err == nil || s.HasTable() {
+		t.Fatal("failed to rollback")
+	}
+}
+
+func transactionCommit(t *testing.T) {
+	engine := OpenDb(t)
+	defer engine.Close()
+	s := engine.NewSession()
+	_ = s.Model(&User{}).DropTable()
+
+	_, err := engine.Transaction(func(s *session.Session) (result interface{}, err error) {
+		_ = s.Model(&User{}).CreateTable()
+		_, err = s.Insert(&User{"Tom", 18})
+		return
+	})
+
+	u := &User{}
+	_ = s.First(u)
+	if err != nil || u.Name != "Tom" {
+		t.Fatal("failed to commit")
+	}
+}
