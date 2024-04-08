8409c22de756e65a49586cbc2656d3b12ebf975a
tianyang <34160429+tianyang-git@users.noreply.github.com>
2024-04-08 23:27:54 +0800

    7days-rpc

---
+++ src/7days-rpc/day3-service/codec/codec.go
@@ -0,0 +1,32 @@
+package codec
+
+import "io"
+
+type Header struct {
+	ServiceMethod string
+	Seq           uint64
+	Error         string
+}
+
+type Codec interface {
+	io.Closer
+	ReadHeader(*Header) error
+	ReadBody(interface{}) error
+	Write(*Header, interface{}) error
+}
+
+type NewCoderFunc func(io.ReadWriteCloser) Codec
+
+type Type string
+
+const (
+	GobType  Type = "application/gob"
+	JsonType Type = "application/json"
+)
+
+var NewCodecFuncMap map[Type]NewCoderFunc
+
+func init() {
+	NewCodecFuncMap = make(map[Type]NewCoderFunc)
+	NewCodecFuncMap[GobType] = NewGobCodec
+}
---
+++ src/7days-rpc/day3-service/go.mod
@@ -0,0 +1,3 @@
+module day3_service
+
+go 1.21
